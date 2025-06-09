e65dd57c3fd7863f291ee3761137ad2a4140ce33
halimfahkri <halimfahkri@gmail.com>
2025-06-09 12:29:47 +0700

    new summit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,27 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact__message')
 
+const sendEmail = (e) =>{
+    e.preventDevault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_kbyiinc','template_mg62wp9','#contact-form','ZLFTsikxYTonmdzw7')
+
+    .then(() =>{
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+           contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+    })
+}
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
